b05a06f99b0a1652003b68a4afc5ee5ee7df1eda
Kyle Begeman <kylebegeman@gmail.com>
2017-04-17 12:40:56 -0700

    Initial Commit

---
+++ WebHeadersController/WebHeadersController.h
@@ -0,0 +1,19 @@
+//
+//  WebHeadersController.h
+//  WebHeadersController
+//
+//  Created by Kyle Begeman on 4/17/17.
+//  Copyright © 2017 Kyle Begeman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebHeadersController.
+FOUNDATION_EXPORT double WebHeadersControllerVersionNumber;
+
+//! Project version string for WebHeadersController.
+FOUNDATION_EXPORT const unsigned char WebHeadersControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebHeadersController/PublicHeader.h>
+
+
